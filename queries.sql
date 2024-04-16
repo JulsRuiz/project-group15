6f4c33c9d0a4ab97caf5bc6f6560486272a1a209
JulsRuiz <165219469+JulsRuiz@users.noreply.github.com>
2024-04-16 15:27:52 -0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+
+--Get the Average adg for each vigor score
 SELECT CAST(alpha_value AS integer) AS vigor, AVG(CAST(overall_adg AS float)) AS average_adg
 FROM Trait LEFT JOIN Goat 
 ON Trait.goat_id=Goat.goat_id
@@ -6,6 +8,8 @@ WHERE trait_code=(
 )
 GROUP BY vigor
 ORDER BY vigor;
+
+--Get the life span of each goat
 
 CREATE VIEW Births AS
 	SELECT goat_id, dob AS date
@@ -17,12 +21,26 @@ CREATE VIEW Deaths AS
 	WHERE status = 'Dead';
 
 CREATE VIEW Lifespan AS
-SELECT Births.goat_id, Deaths.date - Births.date AS Lifespan
-FROM Births JOIN Deaths ON Births.goat_id = Deaths.goat_id;
+	SELECT Births.goat_id, Deaths.date - Births.date AS Lifespan
+	FROM Births JOIN Deaths ON Births.goat_id = Deaths.goat_id;
 
 DROP VIEW Lifespan;
 DROP VIEW Births;
 DROP VIEW Deaths;
 
+--Get the average birth weight for each year
 
+CREATE VIEW BirthWeight AS
+	SELECT goat_id, CAST(alpha_value as float) as weight
+	FROM Trait
+	WHERE trait_code = '357' AND NOT alpha_value = '0.0' AND NOT alpha_value = '-1';
+
+CREATE VIEW BirthYear AS
+	SELECT goat_id, EXTRACT('Year' FROM dob) AS Year
+	FROM Goat;
+
+SELECT Year, ROUND(AVG(weight):: numeric, 3)
+FROM BirthWeight JOIN BirthYear ON BirthWeight.goat_id = BirthYear.goat_id
+GROUP BY Year
+ORDER BY Year;
 
